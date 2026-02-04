d6530b09d1e6af936f4420c2b5a8e394c39d3396
saurav5380 <saurav5380@gmail.com>
2026-02-04 23:04:46 +0530

    landing page added

--- TaskTracker/Frontend/src/app/page.tsx
+++ TaskTracker/Frontend/src/app/page.tsx
@@ -1,65 +1,48 @@
 import Image from "next/image";
-
+import { Database, TrendingUp, Zap, Users, ArrowRight } from 'lucide-react';
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <>
+    <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black border border-b-zinc-50 ">
+      <main className="flex w-full max-w-3xl items-center justify-between py-16 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src="/TaskTracker.svg"
+          alt="Tasktracker logo"
+          width={160}
+          height={40}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        <div className="flex gap-6 items-center justify-between">
+          <button className="px-4 py-4 border border-zinc-50 mt-6  rounded-lg text-xl hover:cursor-pointer hover:text-orange-400 hover:border-orange-400 transition-all">Login</button>
+          <button className="px-4 py-4 border border-zinc-50 mt-6 rounded-lg text-xl hover:cursor-pointer hover:text-orange-400 hover:border-orange-400 transition-all">Signup</button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </main>  
     </div>
+    <div className="flex flex-col w-full">
+        <h2 className="p-4 text-center text-5xl font-extrabold">Organize Your Day, Track Your Progress</h2>
+        <h3 className="p-4 text-center text-2xl font-bold">A simple daily task list app that helps you stay focused and productive</h3>
+    </div>
+    <div className="flex gap-6 justify-center p-4">
+    <button className="px-4 py-4 border border-zinc-50  rounded-lg text-lg hover:cursor-pointer hover:text-orange-400 hover:border-orange-400 transition-all">Get Started Free</button>
+    <button className="px-4 py-4 border border-zinc-50  rounded-lg text-lg hover:cursor-pointer hover:text-orange-400 hover:border-orange-400 transition-all"> Watch Demo</button>
+    </div>
+    <div className="flex items-center justify-center">
+    <div className="relative w-full max-w-lg h-64 flex items-center justify-center m-4">
+      <Image src="/hero.png"
+      alt="Hero Image"
+      layout="fill"
+      objectFit="cover"
+      className="rounded-lg shadow-xl"
+      />
+    </div>
+    </div>
+    <section className="relative w-full max-w-lg">
+      <h2 className="">Features That Keep You Productive</h2>
+      <div>
+
+      </div>
+    </section>
+    </>
   );
 }
