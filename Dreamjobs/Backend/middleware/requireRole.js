87689138036587a348af1e62ad05191c0509c5ae
saurav5380 <saurav5380@gmail.com>
2026-05-15 22:59:20 +0530

    user role check added

---
+++ Dreamjobs/Backend/middleware/requireRole.js
@@ -0,0 +1,14 @@
+
+// middleware to check if the user role stored in DB matches with the provide user role 
+
+const requireRole = (userRole) => {
+    return (req, res, next)=> {
+        const {role} = req.user; 
+        if (userRole !== role){
+            return res.status(403).json({
+                message: "Forbidden Request"
+            })
+        }
+        next();
+    }
+} 
