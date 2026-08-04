4c386ddfba4630eb1c3b144a507e89a57ef9ff5b
saurav5380 <saurav5380@gmail.com>
2026-08-04 15:34:06 +0530

    feat(auth): add guard component

---
+++ Portfolio/SkillKraft/frontend/src/components/layout/AuthGuard.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+// Purpose: reads access token via `auth.ts`. Redirects to `/login` if absent; renders children if present.
+
+import * as authHelpers from '@/lib/auth';
+import { redirect }  from 'next/navigation';
+
+const AuthGuard = ({children}: {children: React.ReactNode}) => {
+    const token = authHelpers.getAccessToken();
+    if (!token){
+        redirect("/auth/login");
+    }
+    return (
+        <>
+        {children}
+        </>
+    )
+}
+
+
+export default AuthGuard;
+
