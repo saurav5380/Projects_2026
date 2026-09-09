4b9b72f109f1f2a2dff38a7dcfb24491b1f6ea22
saurav5380 <saurav5380@gmail.com>
2026-09-09 20:16:10 +0530

    feat(update): data access layer for AI identified resources

--- Portfolio/SkillKraft/backend/src/repositories/resource.repository.ts
+++ Portfolio/SkillKraft/backend/src/repositories/resource.repository.ts
@@ -3,6 +3,7 @@
 import { PrismaClientKnownRequestError } from "../generated/prisma/internal/prismaNamespace.js";
 import prisma from "../db/prisma.js";
 import { ResourceType, ResourceSource, VerificationStatus } from "../generated/prisma/enums.js";
+import type { Resource } from "../generated/prisma/client.js";
 
 export const createResources = async (topicId: string, title: string, url: string | null,
     searchQuery: string, resourceType: ResourceType,
@@ -35,9 +36,154 @@ export const createResources = async (topicId: string, title: string, url: strin
                 "code": error.code
             }
         }
+        else if (error instanceof Error){
+            return (`Error: ${error.message}`)
+        }
     }
 }
 
-export const findTopic = async () => {
+export const findByTopicId = async (id: string) => {
+    const result = await prisma.resource.findFirst({
+        where: {
+            topicId: id
+        }
+    })
+    if (result === null){
+        return (`No topic found. Check the topic ID: ${id}`)
+    }
+    return result;
+}
 
+export const createOne = async (userId: string, topicId:string, title:string, url:string, resourceType:ResourceType) => {
+    try {
+        const result = await prisma.resource.create({
+            data: {
+                topicId,
+                title,
+                url,
+                searchQuery:"",
+                resourceType,
+                source:"USER",
+                verificationStatus:"VERIFIED",
+                flaggedForRevalidation:false,
+                addedByUserId: userId
+            }
+        })
+        return result;
+    }
+    catch (error) {
+        if (error instanceof PrismaClientKnownRequestError) {
+            console.error(`Error: ${error.message}`)
+            return {
+                "error": error.name,
+                "details": error.message,
+                "code": error.code
+            }
+        }
+        else if (error instanceof Error){
+            return (`Error: ${error.message}`)
+        }
+    }
+}
+
+export const deleteById = async (resourceId: string) => {
+    try{
+        const result = await prisma.resource.delete({
+        where: {
+            id: resourceId
+        }
+    })
+    return result;
+    }
+    catch(error){
+        if (error instanceof PrismaClientKnownRequestError){
+         console.error(`Error: ${error.message}`)
+            return {
+                "error": error.name,
+                "details": error.message,
+                "code": error.code
+            }   
+        }
+        else if (error instanceof Error){
+            return (`Error: ${error.message}`)
+        }
+    }
+}
+
+export const updateVerificationStatus = async (resourceId: string, isValidated: VerificationStatus) => {
+    try{
+        const result = await prisma.resource.update({
+        where: {
+            id: resourceId
+        },
+        data: {
+            verificationStatus: isValidated
+        }
+    })
+    return result;
+    }
+    catch(error){
+         if (error instanceof PrismaClientKnownRequestError){
+         console.error(`Error: ${error.message}`)
+            return {
+                "error": error.name,
+                "details": error.message,
+                "code": error.code
+            }   
+        }
+        else if (error instanceof Error){
+            return (`Error: ${error.message}`)
+        }
+    }
+}
+
+export const setFlaggedForRevalidation = async (resourceId: string, revalidationFlag:boolean) => {
+    try{
+        const result = await prisma.resource.update({
+        where: {
+            id: resourceId
+        },
+        data: {
+            flaggedForRevalidation: revalidationFlag
+        }
+    })
+    return result;
+    }
+    catch(error){
+         if (error instanceof PrismaClientKnownRequestError){
+         console.error(`Error: ${error.message}`)
+            return {
+                "error": error.name,
+                "details": error.message,
+                "code": error.code
+            }   
+        }
+         else if (error instanceof Error){
+            return (`Error: ${error.message}`)
+        }
+    }
+}
+
+export const findFlaggedForRevalidation = async (): Promise<Resource[]|{ error: string; details: string; code: string } | string | undefined> => {
+    try{
+        const result = await prisma.resource.findMany({
+        where: {
+            flaggedForRevalidation: true
+        }
+    })
+        return result;
+    }
+    catch(error){
+        if (error instanceof PrismaClientKnownRequestError){
+         console.error(`Error: ${error.message}`)
+            return {
+                "error": error.name,
+                "details": error.message,
+                "code": error.code
+            }   
+        }
+        else if (error instanceof Error){
+            return (`Error: ${error.message}`)
+        }
+    }
 }
